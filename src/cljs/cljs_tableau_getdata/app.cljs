1e7ab0435650c6d42b586b9882ab9adafac1e94e
Tamas Foldi <tfoldi@starschema.net>
2016-08-03 22:02:42 +0200

    Data is coming from tableau

--- src/cljs/cljs_tableau_getdata/app.cljs
+++ src/cljs/cljs_tableau_getdata/app.cljs
@@ -2,7 +2,8 @@
   (:require [reagent.core :as reagent :refer [atom]]
 						[reagent-modals.modals :as reagent-modals]))
 
-(defonce viz (reagent/atom {:status "Loading" :vizobj nil}))
+(defonce viz (reagent/atom {}))
+(declare modal)
 
 ;; Tableau API
 (def viz-url
@@ -13,27 +14,61 @@
   (js-obj
     "hideTabs" true
     "hideToolbar" true
-    "onFirstInteractive" #(swap! viz assoc :status "Ready to interact")))
+    "onFirstInteractive" #(swap! viz assoc :ready true)))
 
-(swap! viz assoc :status "Initializing view..." :vizobj
+(def get-data-options (js-obj "maxRows" 10)) 
+  
+(swap! viz assoc :ready false :vizobj
        (js/tableau.Viz. (.getElementById js/document "tableau-div") viz-url viz-options))
 
-(defn get-sheet
-  "Get the `Sheet` object from the active sheet. Active sheet must be a `Dashboard`"
-  []
+(defn assign-data!
+  "Get the `Sheet` object from the active sheet."
+  [f]
   (-> (:vizobj @viz)
       (.getWorkbook)
-      (.getActiveSheet)))
+      (.getActiveSheet)
+      (f get-data-options) 
+      (.then (fn [data] 
+               (swap! viz assoc :columns (.getColumns data) :data (.getData data))
+               (reagent-modals/modal! [modal] {:size :lg}) ))))
 
 ;; UI 
+(defn modal-render []
+  [:table.table.table-striped.table-bordered 
+   {:cell-spacing "0" :width "100%"}
+
+   [:thead
+	  (reduce (fn [res col] (conj res (vector :th (.getFieldName col)))) [:tr] (:columns @viz))]
+
+    ; tbody
+    (reduce (fn [res row] 
+              (conj res 
+                    (reduce 
+                      (fn [tr col] (conj tr (vector :td (get col "formattedValue")))) 
+                      [:tr] 
+                      row))) 
+            [:tbody] 
+            (js->clj (:data @viz)))])
+
+(defn modal-did-mount [this]
+  (.DataTable (js/$ (reagent/dom-node this))))
+
+
+(defn modal []
+  (reagent/create-class {:reagent-render modal-render
+                         :component-did-mount modal-did-mount}))
+
+(defn get-data-and-show-modal! [f]
+  (assign-data! f))
+
 (defn modal-window-button []
   [:div.btn.btn-primary 
-   {:on-click #(reagent-modals/modal! [:div "some message to the user!"])} 
+   {:disabled (not (:ready @viz))
+    :on-click (fn [] (get-data-and-show-modal! #(.getUnderlyingDataAsync %1)))} 
    "Show underlying data"])
 
 (defn get-data-component []
   [:div
-   [:p (:status @viz)]
    [reagent-modals/modal-window]
    [modal-window-button]])
 
